cd0107cbc58479eba21d39b56471e5a072d26a0e
Zohaib Ehtehsam <zohaib.ehtesham@gmail.com>
2025-04-20 07:37:13 +0100

    Fix Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config} from "../lib/wagmi";
 
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
 // Create a query client
 const queryClient = new QueryClient();
 
@@ -14,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <WagmiProvider config={config}>
           <QueryClientProvider client={queryClient}>
             {children}
